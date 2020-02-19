3afedb2417c8376c3056b0a31cdcda9e543ed208
ajantonio01 <aantonio@student.apc.edu.ph>
2020-02-19 20:38:38 +0800

    Sprint 1 complete

---
+++ homepage.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+<title>Reservation Management System</title>
+<meta charset="utf-8">
+
+<link rel="stylesheet" type="text/css" href="bootstrap.min.css">
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+
+<div class="jumbotron jumbotron-fluid header_background">
+  <div class="container">
+    <ul class="nav justify-content-center">
+  <img src="NUAPC_LOGO.png">
+  <h1 id="between_logo_title">Reservation Management System</h1>
+    </ul>
+  </div>
+</div>
+
+<script src="jquery.slim.min.js"></script>
+<script src="bootstrap.min.js"></script>
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,27 +1,37 @@
 <!doctype html>
 <html lang="en">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <head>
 <title>Reservation Management System</title>
 <meta charset="utf-8">
 
 <link rel="stylesheet" type="text/css" href="bootstrap.min.css">
-<link rel="stylesheet" type="text/css" href="style.css">
+<link rel="stylesheet" type="text/css" href="style2.css">
 </head>
 
 <body>
 
-
-<div class="jumbotron jumbotron-fluid header_background">
-  <div class="container">
-    <ul class="nav justify-content-center">
-  <img src="NUAPC_LOGO.png">
-  <h1 id="between_logo_title">Reservation Management System</h1>
-    </ul>
-  </div>
+<div class="loginbox">
+    <h3>Reservation Management System</h3>
+    <p id="detail"><i>Reserve your rooms here</i></p>
+    <form>
+        <p id="usern_passw">Username</p>
+        <input type="text" name="" placeholder="Enter Username">
+        <p id="usern_passw">Password</p>
+        <input type="password" name="" placeholder="Enter Password">
+        <input id="homepage" class="btn btn-primary" 
+        type="button" name="" value="Login">
+        <a href="#">Don't have an account yet?</a>
+    </form>
 </div>
-
-<script src="jquery.slim.min.js"></script>
+</div>
+<script type="text/javascript">
+    document.getElementById("homepage").onclick = function(){
+        location.href="homepage.php";
+    };
+    </script>
+    <script src="jquery.slim.min.js"></script>
 <script src="bootstrap.min.js"></script>
 </body>
-</html>
+    </html>
